79edbfab61b4b6a2a87a96655f78100772f87803
郑海关 <20262010061@fudan.edu.cn>
2020-11-22 17:45:18 +0800

    上传ruoyi-vue源码

---
+++ ruoyi-ui/src/views/index.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="app-container home">
+    <el-row :gutter="20">
+      <el-col :sm="24" :lg="24">
+        <blockquote class="text-warning" style="font-size: 30px">
+          欢迎登录
+        </blockquote>
+      </el-col>
+    </el-row>
+    <el-col :xs="24" :sm="24" :md="12" :lg="8">
+      <el-card class="update-log">
+        <div slot="header" class="clearfix">
+          <span>联系信息</span>
+        </div>
+        <div class="body">
+          <p>
+            <i class="el-icon-chat-dot-round"></i> 
+            微信：<a>123456789</a>
+          </p>
+        </div>
+      </el-card>
+    </el-col>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index",
+  data() {
+    return {
+      // 版本号
+      version: "3.2.0",
+    };
+  },
+  methods: {
+    goTarget(href) {
+      window.open(href, "_blank");
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.home {
+  blockquote {
+    padding: 10px 20px;
+    margin: 0 0 20px;
+    font-size: 17.5px;
+    border-left: 5px solid #eee;
+  }
+  hr {
+    margin-top: 20px;
+    margin-bottom: 20px;
+    border: 0;
+    border-top: 1px solid #eee;
+  }
+  .col-item {
+    margin-bottom: 20px;
+  }
+
+  ul {
+    padding: 0;
+    margin: 0;
+  }
+
+  font-family: "open sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-size: 13px;
+  color: #676a6c;
+  overflow-x: hidden;
+
+  ul {
+    list-style-type: none;
+  }
+
+  h4 {
+    margin-top: 0px;
+  }
+
+  h2 {
+    margin-top: 10px;
+    font-size: 26px;
+    font-weight: 100;
+  }
+
+  p {
+    margin-top: 10px;
+
+    b {
+      font-weight: 700;
+    }
+  }
+
+  .update-log {
+    ol {
+      display: block;
+      list-style-type: decimal;
+      margin-block-start: 1em;
+      margin-block-end: 1em;
+      margin-inline-start: 0;
+      margin-inline-end: 0;
+      padding-inline-start: 40px;
+    }
+  }
+}
+</style>
